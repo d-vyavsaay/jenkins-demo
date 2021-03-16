19a62dd47a5b734abf05abfc64d534215862451d
Deepak Singh <d.vyavsaay@gmail.com>
2021-03-16 12:27:25 +0530

    modified config for simple micro project

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM jenkins/jenkins:2.271-jdk11
+#FROM jenkins/jenkins:2.271-jdk11
+FROM jenkins/jenkins:2.283-jdk11
 
 COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
 RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('simple-micro') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
